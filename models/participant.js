367dba6562b0fb90fa114624e0abe72e9dbef6a4
Patricia <patriciastefanut@yahoo.com>
2025-03-27 07:38:47 +0200

    add participant model

---
+++ models/participant.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const participantSchema = new mongoose.Schema(
+  {
+    event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+    email: { type: String, required: true },
+    notes: { type: String, default: null },
+    dietaryRestrictions: { type: String, default: null },
+    checkedIn: {type: Boolean, default: false},
+  },
+  {
+    timestamps: true,
+  }
+);
+
+participantSchema.index({ event: -1, email: 1 }, { unique: true });
+
+export default mongoose.model("Participant", participantSchema);
