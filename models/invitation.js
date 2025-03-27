21fbcda179f368c1c1f87dba016cc3b8c7dfff82
Patricia <patriciastefanut@yahoo.com>
2025-03-27 07:29:19 +0200

    add invitation model

---
+++ models/invitation.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const invitationSchema = new mongoose.Schema(
+  {
+    event: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+    email: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "declined"],
+      default: "pending",
+    },
+    token: { type: String, required: true },
+    sendAt: { type: Date, required: true },
+    respondedAt: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Invitation", invitationSchema);
