c552c6fdadf4373a6d4bb427734bddff7e3668b2
Patricia <patriciastefanut@yahoo.com>
2025-03-30 11:12:29 +0300

    add validatiors for user updates

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,35 @@
 import express from "express";
+import { body } from "express-validator";
 import userController from "../controller/userController.js";
 
 const router = express.Router();
 
 router.get("/:userId", userController.getUser);
-router.patch("/:userId", userController.updateUser);
-router.patch("/:userId/password", userController.updatePassword);
+router.patch(
+  "/:userId",
+  [
+    body("firstname")
+      .optional()
+      .notEmpty()
+      .withMessage("Firstname is required"),
+    body("lastname").optional().notEmpty().withMessage("Lastname is required"),
+    body("email")
+      .optional()
+      .isEmail()
+      .withMessage("Email is required")
+      .normalizeEmail(),
+  ],
+  userController.updateUser
+);
+router.patch(
+  "/:userId/password",
+  [
+    body("currentPassword")
+      .notEmpty()
+      .withMessage("Current password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required"),
+  ],
+  userController.updatePassword
+);
 
 export default router;
